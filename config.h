6492a5b649d6c509b6e9414a92cf6e6f5d785231
roman <roman@7bc60720-51fb-0310-bb35-cf765b5d350c>
2003-06-03 14:13:17 +0000

    Committing in OpenScada Modified Files:  	Makefile Makefile.in aclocal.m4 config.cache config.h  	config.h.in config.log config.status configure configure.in  	libtool Added Files:  	depcomp stamp-h1 - Adapted for RHLinux 9.0
    git-svn-id: svn://oscada.org/trunk/OpenScada@130 7bc60720-51fb-0310-bb35-cf765b5d350c

--- config.h
+++ config.h
@@ -4,9 +4,6 @@
 /* Define if you have the ANSI C header files.  */
 #define STDC_HEADERS 1
 
-/* Define if you have the <dlfcn.h> header file.  */
-#define HAVE_DLFCN_H 1
-
 /* Define if you have the <fcntl.h> header file.  */
 #define HAVE_FCNTL_H 1
 
