c00221dea50b188599c953ae890dc1682cb08432
roman <roman@7bc60720-51fb-0310-bb35-cf765b5d350c>
2003-07-28 15:36:37 +0000

    ---------------------------------------------------------------------- Committing in OpenScada/src/moduls/transport Added Files:  	Makefile Makefile.am Makefile.in test_transport.cpp  	test_transport.h ---------------------------------------------------------------------- - rename tprotocol.cpp -> ttransport.cpp - rename tprotocol.h -> ttransport.h - rename module tprotocol -> ttransport
    git-svn-id: svn://oscada.org/trunk/OpenScada@173 7bc60720-51fb-0310-bb35-cf765b5d350c

---
+++ src/moduls/transport/test_transport.cpp
@@ -0,0 +1,99 @@
+/* Test Modul
+** ==============================================================
+*/
+
+#include <getopt.h>
+#include <string>
+
+#include "../../tapplication.h"
+#include "../../tmessage.h"
+#include "test_transport.h"
+
+//============ Modul info! =====================================================
+#define NAME_MODUL  "test_transport"
+#define NAME_TYPE   "Transport"
+#define VERSION     "0.1"
+#define AUTORS      "Roman_Savochenko"
+#define DESCRIPTION "test"
+#define LICENSE     "GPL"
+//==============================================================================
+
+extern "C" TModule *attach( char *FName, int n_mod );
+
+TTransportTest::TTransportTest(char *name) : TModule()
+{
+    NameModul = NAME_MODUL;
+    NameType  = NAME_TYPE;
+    Vers      = VERSION;
+    Autors    = AUTORS;
+    DescrMod  = DESCRIPTION;
+    License   = LICENSE;
+    FileName  = strdup(name);
+
+    ExpFunc   = NULL; // (SExpFunc *)ExpFuncLc;
+    NExpFunc  = 0; // sizeof(ExpFuncLc)/sizeof(SExpFunc);
+#if OSC_DEBUG
+    App->Mess->put( 1, "Run constructor %s file %s is OK!", NAME_MODUL, FileName );
+#endif
+}
+
+TTransportTest::~TTransportTest()
+{
+#if OSC_DEBUG
+    App->Mess->put(1,"Run destructor moduls %s file %s is OK!",NAME_MODUL,FileName);
+#endif
+    free(FileName);	
+}
+
+TModule *attach( char *FName, int n_mod )
+{
+    TTransportTest *self_addr;
+    if(n_mod==0) self_addr = new TTransportTest( FName );
+    else         self_addr = NULL;
+    return ( self_addr );
+}
+
+void TTransportTest::info( const string & name, string & info )
+{
+    info.erase();
+    TModule::info(name,info);
+}
+
+
+
+
+void TTransportTest::pr_opt_descr( FILE * stream )
+{
+    fprintf(stream,
+    "==================== %s options =================================\n"
+    "\n",NAME_MODUL);
+}
+
+
+
+void TTransportTest::CheckCommandLine(  )
+{
+    int next_opt;
+    char *short_opt="h";
+    struct option long_opt[] =
+    {
+	{NULL        ,0,NULL,0  }
+    };
+
+    optind=opterr=0;
+    do
+    {
+	next_opt=getopt_long(App->argc,(char * const *)App->argv,short_opt,long_opt,NULL);
+	switch(next_opt)
+	{
+	    case 'h': pr_opt_descr(stdout); break;
+	    case -1 : break;
+	}
+    } while(next_opt != -1);
+}
+
+void TTransportTest::init( void *param )
+{
+    TModule::init( param );
+}
+
---
+++ src/moduls/transport/test_transport.h
@@ -0,0 +1,25 @@
+#ifndef TEST_TRANSPORT_H
+#define TEST_TRANSPORT_H
+
+#include "../../tmodule.h"
+
+class TTransportTest: public TModule
+{
+    public:
+	TTransportTest(char *name);
+	~TTransportTest();
+	
+	void info( const string & name, string & info );
+       	void init( void *param );
+	
+	void CheckCommandLine(  );
+    public:
+
+    private:
+	void pr_opt_descr( FILE * stream );
+    private:
+	static SExpFunc ExpFuncLc[];	
+};
+
+#endif //TEST_TRANSPORT_H
+
