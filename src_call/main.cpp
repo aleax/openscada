3587da29212e8326f4ea6003c15ff9922fcfc900
roman <roman@7bc60720-51fb-0310-bb35-cf765b5d350c>
2009-01-26 08:19:04 +0000

    2009-01-26 Roman - !!:/SYS: Main call programm building is moved to separated directory src_call/. - FIS:/SYS: AC_PREFIX_DEFAULT value is fixed on file configure.in.
    git-svn-id: svn://oscada.org/trunk/OpenSCADA@814 7bc60720-51fb-0310-bb35-cf765b5d350c

--- src_call/main.cpp
+++ src_call/main.cpp
@@ -19,8 +19,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include <locale.h>
-#include <unistd.h>
 #include <getopt.h>
 
 #include "terror.h"
