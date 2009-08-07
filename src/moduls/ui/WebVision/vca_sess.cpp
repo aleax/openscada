4dfc73111b833b71e49eff0a117e207643d2fd14
roman <roman@7bc60720-51fb-0310-bb35-cf765b5d350c>
2009-08-07 15:17:11 +0000

    M    WebVision/vca_sess.cpp
    git-svn-id: svn://oscada.org/trunk/OpenSCADA@958 7bc60720-51fb-0310-bb35-cf765b5d350c

--- src/moduls/ui/WebVision/vca_sess.cpp
+++ src/moduls/ui/WebVision/vca_sess.cpp
@@ -20,6 +20,8 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <stdint.h>
+
 #include <string.h>
 
 #include <tsys.h>
