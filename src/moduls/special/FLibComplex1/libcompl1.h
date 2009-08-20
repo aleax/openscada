ed38b5b5847bb6b1011c57f803fc79982538c618
roman <roman@7bc60720-51fb-0310-bb35-cf765b5d350c>
2009-08-20 12:26:31 +0000

    M    src/moduls/special/FLibComplex1/libcompl1.h
    git-svn-id: svn://oscada.org/trunk/OpenSCADA@968 7bc60720-51fb-0310-bb35-cf765b5d350c

--- src/moduls/special/FLibComplex1/libcompl1.h
+++ src/moduls/special/FLibComplex1/libcompl1.h
@@ -764,7 +764,7 @@ class SumMult : public TFunction
 	{
 	    return _("Sum and mult per formule:\n"
 		"out = in1_1*in1_2*(in1_3*in1_4+in1_5) + in2_1*in2_2*(in2_3*in2_4+in2_5) +\n"
-		"      in4_1*in4_2*(in4_3*in4_4+in4_5) + in5_1*in5_2*(in5_3*in5_4+in5_5);");
+		"      in3_1*in3_2*(in3_3*in3_4+in3_5) + in4_1*in4_2*(in4_3*in4_4+in4_5);");
 	}
 
 	void calc( TValFunc *v )
@@ -807,7 +807,7 @@ class SumDiv : public TFunction
 	{
 	    return _("Sum and divide per formule:\n"
 		"out = in1_1*in1_2*(in1_3+in1_4/in1_5) + in2_1*in2_2*(in2_3+in2_4/in2_5) +\n"
-		"      in4_1*in4_2*(in4_3+in4_4/in4_5) + in5_1*in5_2*(in5_3+in5_4/in5_5);");
+		"      in4_1*in3_2*(in3_3+in3_4/in3_5) + in4_1*in4_2*(in4_3+in4_4/in4_5);");
 	}
 
 	void calc( TValFunc *v )
