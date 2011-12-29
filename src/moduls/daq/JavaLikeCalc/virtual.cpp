8312a7687eb64971f3bd4787314eba3fec1ad016
roman <roman@7bc60720-51fb-0310-bb35-cf765b5d350c>
2011-12-29 10:24:32 +0000

    M    src/moduls/daq/JavaLikeCalc/virtual.cpp
    git-svn-id: svn://oscada.org/trunk/OpenSCADA@1691 7bc60720-51fb-0310-bb35-cf765b5d350c

--- src/moduls/daq/JavaLikeCalc/virtual.cpp
+++ src/moduls/daq/JavaLikeCalc/virtual.cpp
@@ -185,7 +185,7 @@ void TipContr::compileFuncSynthHighl( const string &lang, XMLNode &shgl )
 {
     if(lang == "JavaScript")
     {
-	shgl.childAdd("rule")->setAttr("expr","\"(\\\\\"|[^\"])*\"")->setAttr("color","darkgreen")->
+	shgl.childAdd("rule")->setAttr("expr","\"([^\\\\]\\\\\"|[^\"])*\"")->setAttr("color","darkgreen")->
 	     childAdd("rule")->setAttr("expr","\\\\([xX][a-zA-Z0-9]{2}|[0-7]{3}|.{1})")->setAttr("color","green")->setAttr("font_weight","1");
 	shgl.childAdd("blk")->setAttr("beg","/\\*")->setAttr("end","\\*/")->setAttr("color","gray")->setAttr("font_italic","1");
 	shgl.childAdd("rule")->setAttr("expr","//[^\n]*")->setAttr("color","gray")->setAttr("font_italic","1");
