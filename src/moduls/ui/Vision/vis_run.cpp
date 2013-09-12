a8f909210f177a0a9c1724ec4b03ec9a234321d5
Alexey Bondarchuk <alexey.bondarchuk@aleax.me>
2013-09-12 10:02:10 +0300

    move joistick to vis_run

--- src/moduls/ui/Vision/vis_run.cpp
+++ src/moduls/ui/Vision/vis_run.cpp
@@ -1139,6 +1139,44 @@ void VisRun::initSess( const string &prj_it, bool crSessForce )
 
     //> Start timer
     updateTimer->start(period());
+
+#ifdef HAVE_SDL
+    // Init libsdl
+    bool sdl_init=false;
+    // Sure, we're only using the Joystick, but SDL doesn't work if video isn't initialised
+    if ( SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) != -1 )
+    {
+    	sdl_init=true;
+//#if OSC_DEBUG >= 1
+    	mess_info(__func__, "SDL initialization successful");
+    	// Prints the compile time version
+    	SDL_version ver;
+    	SDL_VERSION(&ver);
+    	mess_info(__func__, "SDL compile-time version: %u.%u.%u", ver.major, ver.minor, ver.patch);
+    	ver = *SDL_Linked_Version();
+    	mess_info(__func__, "SDL runtime version: %u.%u.%u", ver.major, ver.minor, ver.patch);
+//#endif
+    }
+    else
+    {
+    	mess_err(__func__, "SDL initialization error: %s", SDL_GetError());
+    	sdl_init=false;
+    }
+
+    if(sdl_init)
+    {
+    	int numJoy=SDL_NumJoysticks();
+    	for(int i=0; i< numJoy; i++)
+    	{
+    		SDLJoystick *Joystick = new SDLJoystick(i, this->master_pg);
+    		connect(Joystick, SIGNAL(finished()),
+    				Joystick, SLOT(deleteLater()));
+    		Joystick->start();
+    	}
+    }
+
+#endif
+
 }
 
 void VisRun::fullUpdatePgs( )
@@ -1368,7 +1406,7 @@ void VisRun::cacheResSet( const string &res, const string &val )
 void VisRun::updatePage( )
 {
     if(winClose) return;
-
+    //mess_debug(__func__,"master_pg 0x%x", master_pg);	//DEBUG
     int rez;
 
 #if OSC_DEBUG >= 3
