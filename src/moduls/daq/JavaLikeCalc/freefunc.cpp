c20fd604cc199220b04a95699c3f925367b2b3d5
roman <roman@7bc60720-51fb-0310-bb35-cf765b5d350c>
2015-01-23 10:58:41 +0000

    M    src/moduls/daq/JavaLikeCalc/Makefile.am M    src/moduls/daq/JavaLikeCalc/freefunc.cpp
    git-svn-id: svn://oscada.org/trunk/OpenSCADA@2197 7bc60720-51fb-0310-bb35-cf765b5d350c

--- src/moduls/daq/JavaLikeCalc/freefunc.cpp
+++ src/moduls/daq/JavaLikeCalc/freefunc.cpp
@@ -731,7 +731,7 @@ Reg *Func::cdBinaryOp( Reg::Code cod, Reg *op1, Reg *op2 )
 	switch(op1->vType(this)) {
 	    case Reg::Int:
 		switch(cod) {
-		    case Reg::RstI:	*op1 = op2->val().i ? (op1->val().i % op2->val().i) : INT64_MAX;	break;
+		    case Reg::RstI:	*op1 = op2->val().i ? (op1->val().i % op2->val().i) : 0;	break;
 		    case Reg::BitOr:	*op1 = op1->val().i | op2->val().i;	break;
 		    case Reg::BitAnd:	*op1 = op1->val().i & op2->val().i;	break;
 		    case Reg::BitXor:	*op1 = op1->val().i ^ op2->val().i;	break;
@@ -1937,7 +1937,7 @@ void Func::exec( TValFunc *val, const uint8_t *cprg, ExecData &dt )
 		if(mess_lev() == TMess::Debug) mess_debug(nodePath().c_str(), "CODE: %d = %d %% %d.", ptr->rez, ptr->a1, ptr->a2);
 #endif
 		int64_t div = getValI(val, reg[ptr->a2]);
-		reg[ptr->rez] = div ? (getValI(val,reg[ptr->a1]) % div) : INT64_MAX;
+		reg[ptr->rez] = div ? (getValI(val,reg[ptr->a1]) % div) : 0;
 		cprg += sizeof(SCode); continue;
 	    }
 	    case Reg::BitOr: {
