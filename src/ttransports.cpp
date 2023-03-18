74d56dec2f08ec788fb663947102e645a87cad84
roman <roman@7bc60720-51fb-0310-bb35-cf765b5d350c>
2023-03-18 10:23:33 +0000

    2023-03-18 Roman (stable) - FIX:Transports: Fixing the building for old environments.
    git-svn-id: svn://oscada.org/trunk/OpenSCADA@2875 7bc60720-51fb-0310-bb35-cf765b5d350c

--- src/ttransports.cpp
+++ src/ttransports.cpp
@@ -18,6 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <errno.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
